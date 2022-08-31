97c12fe4bfb54c377e06093bb52e62ad223027ee
indiepopart <jimena@gmail.com>
2022-08-31 18:38:57 -0300

    initial commit

---
+++ src/main/java/com/okta/developer/alert/service/AlertConsumer.java
@@ -0,0 +1,45 @@
+package com.okta.developer.alert.service;
+
+import com.okta.developer.alert.config.KafkaStoreAlertConsumer;
+import com.okta.developer.alert.domain.StoreAlert;
+import com.okta.developer.alert.repository.StoreAlertRepository;
+import com.okta.developer.alert.service.dto.StoreAlertDTO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.messaging.Message;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+
+@Service
+public class AlertConsumer {
+
+    private final Logger log = LoggerFactory.getLogger(AlertConsumer.class);
+
+    private StoreAlertRepository storeAlertRepository;
+
+    private EmailService emailService;
+
+    public AlertConsumer(StoreAlertRepository storeAlertRepository, EmailService emailService) {
+        this.storeAlertRepository = storeAlertRepository;
+        this.emailService = emailService;
+    }
+
+    @StreamListener(value = KafkaStoreAlertConsumer.CHANNELNAME, copyHeaders = "false")
+    public void consume(Message<StoreAlertDTO> message) {
+        log.debug("Got message from kafka stream: {}", message.getPayload());
+        try {
+            StoreAlertDTO dto = message.getPayload();
+            StoreAlert storeAlert = new StoreAlert();
+            storeAlert.setStoreName(dto.getStoreName());
+            storeAlert.setStoreStatus(dto.getStoreStatus());
+            storeAlert.setTimestamp(Instant.now());
+
+            storeAlertRepository.save(storeAlert);
+            emailService.sendSimpleMessage(dto);
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+        }
+    }
+}
